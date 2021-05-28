correct loss sign

modules/loss_function.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 
 
 def regularization(mean, log_var):
-    return .5 * tf.math.reduce_sum(1 + log_var - tf.math.square(mean) - tf.math.exp(log_var), axis=1)
+    return .5 * tf.math.reduce_sum(log_var -
+                                   tf.math.square(mean) - tf.math.exp(log_var), axis=1)
 
 
 def itakura_saito_div(S_sq, log_var_out):
@@ -12,12 +13,12 @@ def itakura_saito_div(S_sq, log_var_out):
     S_sq (tf.data.Dataset): element-wise norm of complex STFT domain clean speech (real number)
     log_var_out (output VAE): VAE output (real number)
     '''
-    var_out = tf.math.exp(log_var_out)
-    return tf.math.reduce_sum(S_sq/var_out - tf.math.log(S_sq/var_out) - 1, axis=1)
+    return tf.math.reduce_sum(
+        S_sq/tf.math.exp(log_var_out) - tf.math.log(S_sq) + log_var_out - 1, axis=1)
 
 
 def itakura_saito_elbo(S_sq, log_var_out, mean, log_var):
-    ''' Clean speech loss
+    ''' Clean speech loss, eq. (21)
 
     Parameters:
     S_sq (tf.data.Dataset): element-wise norm of complex STFT domain clean speech (real numbers)
@@ -25,4 +26,6 @@ def itakura_saito_elbo(S_sq, log_var_out, mean, log_var):
     mean (output Encoder): Encoder mean (real numbers)
     log_var (output Encoder): Encoder log-variance (real numbers)
     '''
-    return tf.math.reduce_mean(regularization(mean, log_var) - itakura_saito_div(S_sq, log_var_out))
+    loss = tf.math.reduce_mean(regularization(
+        mean, log_var) - itakura_saito_div(S_sq, log_var_out))
+    return -loss