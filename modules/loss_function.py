203889e34e12aaeb2b38141139cc8783f70f42aa
BLyndon <47363948+BLyndon@users.noreply.github.com>
2021-05-30 16:58:43 +0200

    replace loss by equivalent mathematical expression

--- modules/loss_function.py
+++ modules/loss_function.py
@@ -2,8 +2,14 @@ import tensorflow as tf
 
 
 def regularization(mean, log_var):
-    return .5 * tf.math.reduce_sum(log_var -
-                                   tf.math.square(mean) - tf.math.exp(log_var), axis=1)
+    ''' KL-Divergence
+    '''
+    return .5 * tf.math.reduce_sum(
+        log_var
+        - tf.math.square(mean)
+        - tf.math.exp(log_var),
+        axis=1
+    )
 
 
 def itakura_saito_div(S_sq, log_var_out):
@@ -14,7 +20,10 @@ def itakura_saito_div(S_sq, log_var_out):
     log_var_out (output VAE): VAE output (real number)
     '''
     return tf.math.reduce_sum(
-        S_sq/tf.math.exp(log_var_out) - tf.math.log(S_sq) + log_var_out - 1, axis=1)
+        log_var_out +
+        S_sq/(2*tf.math.exp(log_var_out)),
+        axis=1
+    )
 
 
 def itakura_saito_elbo(S_sq, log_var_out, mean, log_var):
@@ -26,6 +35,5 @@ def itakura_saito_elbo(S_sq, log_var_out, mean, log_var):
     mean (output Encoder): Encoder mean (real numbers)
     log_var (output Encoder): Encoder log-variance (real numbers)
     '''
-    loss = tf.math.reduce_mean(regularization(
-        mean, log_var) - itakura_saito_div(S_sq, log_var_out))
-    return -loss
+
+    return itakura_saito_div(S_sq, log_var_out) - regularization(mean, log_var)
