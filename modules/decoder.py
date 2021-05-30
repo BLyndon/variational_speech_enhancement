c767b18f617cb98d13d7f752c2e47931f6ca46e4
BLyndon <47363948+BLyndon@users.noreply.github.com>
2021-05-30 16:54:48 +0200

    remove unnecessary architectures

--- modules/decoder.py
+++ modules/decoder.py
@@ -13,64 +13,42 @@ class Decoder(Layer):
 
     def __init__(self,
                  latent_dim=8,
-                 outp_shape=(513, 1),
-                 net='clean_speech',
+                 outp_shape=513,
+                 hidden_dim=128,
+                 activation='tanh',
                  info=False,
                  **kwargs):
         super(Decoder, self).__init__(**kwargs)
 
         # Initialize Shape & Architecture
-        self.outp_shape = outp_shape
+        if type(outp_shape) == int:
+            self.outp_shape = (outp_shape,)
+        else:
+            self.outp_shape = outp_shape
         self.latent_dim = latent_dim
-        self.net = net
-        assert self.net in ['conv', 'mlp', 'clean_speech'], print(
-            'Choose net from {"conv", "mlp", "clean_speech"}')
+        self.hidden_dim = hidden_dim
+        self.activation = activation
 
         # Initialize Layers
-        if self.net == 'conv':
-            self.dense = Dense(7*7*32, activation='relu')
-            self.reshape = Reshape((7, 7, 32))
-            self.conv2dT1 = Conv2DTranspose(
-                filters=64, kernel_size=3, strides=2, padding='same', activation='relu')
-            self.conv2dT2 = Conv2DTranspose(
-                filters=32, kernel_size=3, strides=2, padding='same',  activation='relu')
-            self.out = Conv2DTranspose(
-                filters=1, kernel_size=3, padding='same', name='output')
-        elif self.net == 'mlp':
-            digit_len = self.outp_shape[0] * \
-                self.outp_shape[1]*self.outp_shape[2]
-            self.dense1 = Dense(512, activation='relu')
-            self.dense2 = Dense(digit_len)
-            self.out = Reshape(self.outp_shape)
-        elif self.net == 'clean_speech':
-            self.dense = Dense(128, activation='tanh')
-            self.out = Dense(self.outp_shape[0])
+        self.dense = Dense(self.hidden_dim, activation=self.activation)
+        self.out = Dense(self.outp_shape[0], name='log_var_f')
 
         # Print Encoder-Decoder Info
         if info == True:
             self.print_info()
 
     def __call__(self, z):
-        if self.net == 'conv':
-            z = self.dense(z)
-            z = self.reshape(z)
-            cz = self.conv2dT1(z)
-            cz = self.conv2dT2(cz)
-            x = self.out(cz)
-        elif self.net == 'mlp':
-            z = self.dense1(z)
-            z = self.dense2(z)
-            x = self.out(z)
-        elif self.net == 'clean_speech':
-            z = self.dense(z)
-            x = self.out(z)
+        z = self.dense(z)
+        x = self.out(z)
         return x
 
     def print_info(self):
         print()
-        print(self.net + '-Decoder')
+        print('Decoder')
+        print(' - Output dimension: {}'.format(self.outp_shape))
+        print(' - Hidden dimension: {}'.format(self.hidden_dim))
         print(' - Latent Variable Space: {}'.format(self.latent_dim))
-        print(' - Output Shape: {}'.format(self.outp_shape))
+        print(' - Activation-fct: {}'.format(self.activation))
 
 
 if __name__ == "__main__":
--- modules/encoder.py
+++ modules/encoder.py
@@ -15,55 +15,46 @@ class Encoder(Layer):
     def __init__(self,
                  inp_shape=(513, 1),
                  latent_dim=8,
-                 net='clean_speech',
+                 hidden_dim=128,
+                 activation='tanh',
                  info=False,
                  **kwargs):
         super(Encoder, self).__init__(**kwargs)
 
         # Initialize Shape & Architecture
-        self.inp_shape = inp_shape
+        if type(inp_shape) == int:
+            self.inp_shape = (inp_shape,)
+        else:
+            self.inp_shape = inp_shape
         self.latent_dim = latent_dim
-        self.net = net
-        assert self.net in ['conv', 'mlp', 'clean_speech'], print(
-            'Choose net from {"conv", "mlp", "clean_speech"}')
+        self.hidden_dim = hidden_dim
+        self.activation = activation
 
         # Initialize Layers
         self.flatten = Flatten()
-        if self.net == 'conv':
-            self.conv2d1 = Conv2D(filters=32, kernel_size=3,
-                                  strides=(2, 2), activation='relu')
-            self.conv2d2 = Conv2D(filters=64, kernel_size=3,
-                                  strides=(2, 2), activation='relu')
-        elif self.net == 'mlp':
-            self.dense_hidden = Dense(512, activation='relu')
-        elif self.net == 'clean_speech':
-            self.dense_hidden = Dense(128, activation='tanh')
-        self.dense_mu = Dense(self.latent_dim, name='mu')
-        self.dense_log_var = Dense(self.latent_dim, name='log_var')
+        self.dense_hidden = Dense(self.hidden_dim, activation=self.activation)
+
+        self.dense_mu = Dense(self.latent_dim, name='mu_l')
+        self.dense_log_var = Dense(self.latent_dim, name='log_var_l')
 
         if info == True:
             self.print_info()
 
     def __call__(self, x):
-        if self.net == 'conv':
-            cx = self.conv2d1(x)
-            cx = self.conv2d2(cx)
-            x = self.flatten(cx)
-        elif self.net == 'clean_speech':
-            x = self.flatten(x)
-            x = self.dense_hidden(x)
-        elif self.net == 'mlp':
-            x = self.flatten(x)
-            x = self.dense_hidden(x)
+        x = self.flatten(x)
+        x = self.dense_hidden(x)
+
         mu = self.dense_mu(x)
         log_var = self.dense_log_var(x)
         return mu, log_var
 
     def print_info(self):
         print()
-        print(self.net + '-Encoder')
-        print(' - Input Shape: {}'.format(self.inp_shape))
+        print('-Encoder')
+        print(' - Output Shape: {}'.format(self.inp_shape))
+        print(' - Hidden dimension: {}'.format(self.hidden_dim))
         print(' - Latent Variable Space: {}'.format(self.latent_dim))
+        print(' - Activation-fct: {}'.format(self.activation))
 
 
 if __name__ == "__main__":
